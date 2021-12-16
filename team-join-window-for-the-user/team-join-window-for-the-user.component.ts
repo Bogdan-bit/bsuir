6936037dd99ae3f307dc8f24fcf4df942961d83b
Bogdan-bit <56504597+Bogdan-bit@users.noreply.github.com>
2021-12-16 14:52:27 -0800

    Add files via upload

---
+++ team-join-window-for-the-user/team-join-window-for-the-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-join-window-for-the-coach',
+  templateUrl: './team-join-window-for-the-coach.component.html',
+  styleUrls: ['./team-join-window-for-the-coach.component.scss']
+})
+export class TeamJoinWindowForTheCoachComponent {
+  private user: userList;
+
+  public toShow() {}
+  public requestTojoin() {}
+  public acceptRequest() {}
+}
